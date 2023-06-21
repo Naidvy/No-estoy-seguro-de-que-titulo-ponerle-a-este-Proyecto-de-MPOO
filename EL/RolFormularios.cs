bd2620ee421aa037a25b99ae05fa173196ee07c5
Norman <Norman@SALLY>
2023-06-20 19:56:56 -0600

    Add project files.

---
+++ EL/RolFormularios.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EL
+{
+    [Table("RolFormularios")]
+    public class RolFormularios
+    {
+        [Key]
+        public int IdRolFormulario { get; set; }
+        public short IdRol { get; set; }
+        public int IdFormulario { get; set; }
+        [Required]
+        public bool Activo { get; set; }
+        [Required]
+        public int IdUsuarioRegistra { get; set; }
+        [Required]
+        public DateTime FechaRegistro { get; set; }
+        public int IdUsuarioActualiza { get; set; }
+        public DateTime FechaActualizacion { get; set; }
+    }
+}
---
+++ EL/Roles.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EL
+{
+    [Table("Roles")]
+    public class Roles
+    {
+        [Key]
+        public short IdRol { get; set; }
+        [MaxLength(50)]
+        [Required]
+        public string Rol { get; set; }
+        [Required]
+        public bool Activo { get; set; }
+        [Required]
+        public int IdUsuarioRegisra { get; set; }
+        [Required]
+        public DateTime FechaRegistro { get; set; }
+        public int IdUsuarioActualiza { get; set; }
+        public DateTime FechaActualizacion { get; set; }
+
+        public Roles()
+        {
+            Rol = string.Empty;
+        }
+    }
+}
