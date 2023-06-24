42ccf50a58df313e5289a6d8fffef6a7da2c4dca
Norman Rodríguez <rodriguez.norman@outlook.com>
2023-06-23 19:29:22 -0600

    .

--- DAL/Conexion.cs
+++ DAL/Conexion.cs
@@ -1,12 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DAL
 {
-    internal class Conexion
+    public static class Conexion
     {
+        private static string NombreAplicacion = "GeneradorCapas";
+        private static string Servidor = @"Norman\SALLY";
+        private static string Usuario = "";
+        private static string Password = "";
+        private static string BaseDatos = "MPOO";
+
+        public static string ConexionString (bool SqlAutentication = true)
+        {
+            SqlConnectionStringBuilder ConstructorCadenaConexion = new()
+            {
+                ApplicationName = NombreAplicacion,
+                IntegratedSecurity = !SqlAutentication,
+                DataSource = Servidor,
+                InitialCatalog = BaseDatos
+            };
+            if (SqlAutentication)
+    {
+                ConstructorCadenaConexion.UserID = Usuario;
+                ConstructorCadenaConexion.Password = Password;
+            }
+            return ConstructorCadenaConexion.ConnectionString;
+        }
     }
 }
